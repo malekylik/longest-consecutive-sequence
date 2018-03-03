62b7c447ed50b5c0d6d119a7f036fc394ed346c1
malekylik <malekylik@gmail.com>
2018-03-03 23:20:23 +0300

    master/fix remove the comments

--- src/index.js
+++ src/index.js
@@ -24,34 +24,3 @@ module.exports = function longestConsecutiveLength(array) {
 
  return count + 1;
 }
-
-
-// function longestConsecutiveLength(array) {
-
-//   let mass = [];
-//   for(let i = 0; i < array.length;i++){
-//     mass[array[i]] = 1;
-//   }
-
-//   let count = 0;
-//   let tempCount = 0;
- 
-//   for(let i = 0; i < mass.length - 1;i++){
-//     if(mass[i] != undefined && mass[i+1] != undefined){
-//       tempCount++;
-//     }else{
-//       if(tempCount > count) count = tempCount;
-
-//       tempCount = 0;
-//       continue;
-//     }
-//   }
-
-
-//  return count + 1;
-  
-// }
-
-
-
-// longestConsecutiveLength([100, 4, 200, 1, 3, 2]);
